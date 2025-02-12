db8ce06715c8118055f811d078400e3e3182b71d
Sushobhon Sahoo <sushobhons@gmail.com>
2025-02-12 21:43:19 +0530

    added CSRF and validation

--- routes/web.php
+++ routes/web.php
@@ -10,17 +10,35 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->cursorPaginate(3);
-    return view('jobs', [
+    $jobs = Job::with('employer')->latest()->simplePaginate(3);
+    return view('jobs.index', [
         'jobs' => $jobs
     ]);
+});
+
+
+Route::get('jobs/create', function () {
+    return view('jobs.create');
 });
 
 Route::get('/jobs/{id}', function ($id) {
 
     $job = Job::find($id);
 
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
+});
+
+Route::post('/jobs', function () {
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required'],
+    ]);
+   Job::create([
+       'title' => request('title'),
+       'salary' => request('salary'),
+       'employer_id' => 1,
+   ]);
+   return redirect('/jobs');
 });
 
 Route::get('/contact', function () {
