d9e331be96a88ee30f6c5287d36c1b16230a39cb
Sushobhon Sahoo <sushobhons@gmail.com>
2025-02-12 16:39:07 +0530

    add eager loading and N + 1 problem

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    /** @use HasFactory<\Database\Factories\TagFactory> */
+
     use HasFactory;
+
+    public function jobs(){
+        return $this->belongsToMany(Job::class, relatedPivotKey:"job_listing_id");
+    }
 }
