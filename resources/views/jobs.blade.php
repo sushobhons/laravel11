77ee1678349625a2425e4e96e8d80aa290708f72
Sushobhon Sahoo <sushobhons@gmail.com>
2025-02-12 19:08:40 +0530

    add pagination and database seeders

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -11,5 +11,10 @@
                 </div>
             </a>
         @endforeach
+
+        <div>
+            {{ $jobs->links() }}
+        </div>
     </div>
+
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->get();
+    $jobs = Job::with('employer')->cursorPaginate(3);
     return view('jobs', [
         'jobs' => $jobs
     ]);
