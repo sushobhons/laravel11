efe0bc261e2f2e360912623f35a88001a35f1ce0
Sushobhon Sahoo <sushobhons@gmail.com>
2025-02-14 00:49:35 +0530

    send email and add queue

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\JobPosted;
 use App\Models\Job;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Mail;
 
 class JobController extends Controller
 {
@@ -36,11 +38,15 @@ class JobController extends Controller
             'title' => ['required', 'min:3'],
             'salary' => ['required'],
         ]);
-        Job::create([
+        $job = Job::create([
             'title' => request('title'),
             'salary' => request('salary'),
             'employer_id' => 1,
         ]);
+
+        Mail::to($job->employer->user)->queue(
+            new JobPosted($job)
+        );
         return redirect('/jobs');
     }
 
