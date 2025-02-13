d0832f3fb94b1c2f9eddd337a82d5b171a2f475d
Sushobhon Sahoo <sushobhons@gmail.com>
2025-02-13 18:16:09 +0530

    add registration and login system

---
+++ app/Http/Controllers/SessionController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class SessionController extends Controller
+{
+    public function create()
+    {
+        return view('auth.login');
+    }
+
+    public function store(Request $request)
+    {
+        $attributes = request()->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if(!Auth::attempt($attributes)){
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        request()->session()->regenerate();
+
+        return redirect('/jobs');
+    }
+    public function destroy(){
+
+        Auth::logout();
+
+        return redirect('/');
+    }
+}
